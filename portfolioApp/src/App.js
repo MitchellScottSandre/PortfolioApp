239a822d8978e3e2359177efb3737e9109450217
MitchellScottSandre <msandre@edu.uwaterloo.ca>
2018-02-11 14:30:53 -0500

    first react native app commit

---
+++ portfolioApp/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+const App = () => {
+    return (
+        <Provider store={createStore()}>
+            <View style={{ flex: 1 }}>
+                <Text>Hello</Text>
+            </View>
+        </Provider>
+    )
+}
+
+export default App
