67e687450ebdaf05fc06da9fec8bb670ce82ee22
Scott Sandre <scottsandre@Scotts-MacBook-Pro.local>
2018-10-13 17:08:02 -0400

    got it started on new laptop...

--- portfolioApp/index.js
+++ portfolioApp/index.js
@@ -1,16 +1,3 @@
-// import { Navigation } from 'react-native-navigation'
-// import App from './src/App'
-
-// AppRegistry.registerComponent('portfolioApp', () => App)
-
-// Navigation.registerComponent('portfolioApp', () => App)
-// Navigation.startSingleScreenApp({
-//   screen: {
-//     screen: 'portfolioApp',
-//     title: 'Welcome'
-//   }
-// })
-
 import App from './src/App'
 
 const app = new App()
